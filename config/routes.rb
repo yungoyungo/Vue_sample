739fd728ad170f82f0754e72ec90357a9a689681
yusa <l.r.play.gvig.wav@gmail.com>
2017-11-17 15:23:37 +0900

    feature/app-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'page/home'
+  root to: 'home#index'
+  #get 'page/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
